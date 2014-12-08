28fdae3b917ed24bab456b85d014eecbfef83b27
Ro0t Da3m0n <amarsharma.hacker@gmail.com>
2014-12-08 19:28:52 +0530

    Quick sort to take input First line as N and int next line array Starting work on AVL Tree

--- Algorithms/Sorting/QuickSort.cpp
+++ Algorithms/Sorting/QuickSort.cpp
@@ -1,52 +1,57 @@
 /* This program contains sample QuickSort program
  * in c++.
- * */
+ */
 #include <iostream>
 
 using namespace std;
 
-void swap(int *a , int *b)
+int count = 0;
+
+void swaps(int *a , int *b)
 {
-	int temp;
-	temp = *a;
-	*a = *b;
-	*b = temp;
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int Partition(int a[], int l, int r)
 {
-	int pivot = a[l];
-	int left = l, right = r;
-	while (left < right)
-	{
-		while (a[left] <= pivot)
-			left++;
-		while (a[right] > pivot)
-			right--;
-		if (left < right)
-			swap(a[left], a[right]);
-	}
-	swap(a[l],a[right]);
-	return right;
+    int pivot = a[l];
+    int left = l, right = r;
+    while (left < right)
+    {
+        while (a[left] <= pivot)
+            left++;
+        while (a[right] > pivot)
+            right--;
+        if (left < right)
+            swaps(&a[left], &a[right]);
+    }
+    swaps(&a[l], &a[right]);
+    return right;
 }
 
 void QuickSort(int a[], int left, int right)
 {
-	int partition;
-	if (left < right)
-	{
-		partition = Partition(a, left, right);
-		QuickSort(a, left, partition - 1);
-		QuickSort(a, partition + 1, right);
-	}
+    int partition;
+    if (left < right)
+    {
+        partition = Partition(a, left, right);
+        QuickSort(a, left, partition - 1);
+        QuickSort(a, partition + 1, right);
+    }
 }
 
 int main()
 {
-	int array[] = {23, 56, -4, 23, -123, 2133, 44};
-	int i;
-	QuickSort(array, 0, 6);
-	for (i = 0; i < 7; i++)
-		cout << " " << array[i];
-	cout << endl;
+    int N;
+    cin >> N;
+    int *array = new int[N];
+    for (int i = 0; i < N ; i++)
+        cin >> array[i];
+    QuickSort(array, 0, N - 1);
+    for (int i = 0; i < N; i++)
+        cout << " " << array[i];
+    cout << endl << count;
 }
